5b0d8ae9e57c68ac45146745397c43c7d9fa5c78
Pavel <pavel170@yahoo.com>
2024-04-03 22:10:55 +0300

    init

---
+++ app.py
@@ -0,0 +1,9 @@
+from flask import Flask, render_template
+
+app = Flask(__name__, static_folder="dist/static", template_folder="dist", static_url_path="/static")
+
+
+@app.route("/", defaults={"path": ""})
+@app.route("/<path:path>")
+def index(path):
+    return render_template("index.html")
